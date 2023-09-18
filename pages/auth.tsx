d44576d578e621cc5d4ee54784fa8ebe65724cdc
Felipe Fardo <felipe.fardo@hotmail.com.br>
2023-09-18 03:11:33 -0300

    feat: add NextAuth, Prisma and Mongo setup

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,11 @@
 import Input from "@/components/Input";
 import { useCallback, useState } from "react";
+import axios from 'axios';
+import {signIn } from 'next-auth/react'
+import { useRouter } from "next/router";
 
 const Auth = () => {
+  const router = useRouter()
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
@@ -10,7 +14,34 @@ const Auth = () => {
 
   const toggleVariant = useCallback(() => {
     setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
-  }, [])
+  }, []);
+  const login = useCallback(async()=>{
+    try {
+      await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: '/'
+      });
+      router.push('/')
+    } catch (error) {
+      console.log(error);
+    }
+  },[email, password,router])
+
+
+  const register = useCallback(async()=>{
+    try {
+      await axios.post('/api/register', {
+        email,
+        name,
+        password
+      });
+      login();
+    } catch (error) {
+      console.log(error)
+    }
+  },[email, name, password, login])
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -47,7 +78,7 @@ const Auth = () => {
                 value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button onClick={variant === 'login'? login: register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
               {variant === 'login' ? 'login' : 'Sign up'}
             </button>
             <p className="text-neutral-500 mt-12">
@@ -55,7 +86,7 @@ const Auth = () => {
                 'First time using Netflix?' : 'Already have an account?'
               }
               <span onClick={toggleVariant} className="text-white ml-1 hover-underline cursor-pointer">
-                {variant === 'login' ? 'Create and account.' : 'Login.'}
+              {variant === 'login' ? 'Create and account.' : 'Login.'}
               </span>
             </p>
           </div>
