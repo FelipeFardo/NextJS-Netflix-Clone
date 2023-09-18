4a04ee11af5603cb5ba73dad017268bc29f3a378
FelipeFardo <felipe.fardo@hotmail.com.br>
2023-09-18 14:11:02 -0300

    feat: add protecting routes and profiles screen

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,13 +2,12 @@ import Input from "@/components/Input";
 import { useCallback, useState } from "react";
 import axios from 'axios';
 import {signIn } from 'next-auth/react'
-import { useRouter } from "next/router";
+
 
 import { FcGoogle} from 'react-icons/fc';
 import { FaGithub} from 'react-icons/fa';
 
 const Auth = () => {
-  const router = useRouter()
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
@@ -23,14 +22,12 @@ const Auth = () => {
       await signIn('credentials', {
         email,
         password,
-        redirect: false,
-        callbackUrl: '/'
+        callbackUrl: '/profiles'
       });
-      router.push('/')
     } catch (error) {
       console.log(error);
     }
-  },[email, password,router])
+  },[email, password])
 
 
   const register = useCallback(async()=>{
@@ -86,7 +83,7 @@ const Auth = () => {
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
-              onClick={()=> signIn('google',{ callbackUrl:'/'})}
+              onClick={()=> signIn('google',{ callbackUrl:'/profiles'})}
               className="
               w-10
               h-10
@@ -102,7 +99,7 @@ const Auth = () => {
                 <FcGoogle size={30}></FcGoogle>
               </div>
               <div 
-              onClick={()=> signIn('github',{ callbackUrl:'/'})}
+              onClick={()=> signIn('github',{ callbackUrl:'/profiles'})}
               className="
               w-10
               h-10
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
+import { getSession, signOut } from "next-auth/react";
+import { NextPageContext } from "next/types";
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
+  const { data: user } = useCurrentUser();
   return (
     <>
       <h1 className="text-2xl text-red-500">Netflix CLone</h1>
+      <p className="text-white">Logged in as: {user?.name}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Logout!
+      </button>
     </>
-  )
+  );
 }
