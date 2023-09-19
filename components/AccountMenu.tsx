7fb43e81bcfa593c166288d3f695d884f8569f46
Felipe Fardo <felipe.fardo@hotmail.com.br>
2023-09-19 00:35:33 -0300

    feat: add user name AccountMenu

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,3 +1,4 @@
+import useCurrentUser from '@/hooks/useCurrentUser';
 import {signOut} from 'next-auth/react';
 import React from 'react';
 
@@ -8,6 +9,7 @@ interface AccountMenuProps {
 const AccountMenu : React.FC<AccountMenuProps>= ({
 	visible
 })=>{
+	const { data: user } = useCurrentUser();
 	if (!visible){
 		return null;
 	}
@@ -16,7 +18,7 @@ const AccountMenu : React.FC<AccountMenuProps>= ({
 				<div className='flex flex-col gap-3'>
 					<div className='px-3 group/item flex flex-row gap-3 items-center w-full'>
 						<img className='w-8 rounded-md' src="/images/default-blue.png" alt="" />
-						<p className='text-white text-sm group-hover/item:underline'>Username</p>
+						<p className='text-white text-sm group-hover/item:underline'>{user?.name}</p>
 					</div>
 					<hr className='bg-gray-600 border-0 h-px my-4'/>
 					<div onClick={()=> signOut()} className='px-3 text-center text-white text-sm hover:underline'>
